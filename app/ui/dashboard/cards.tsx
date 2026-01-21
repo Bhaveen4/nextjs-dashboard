ceaf7c1d2fe993f9f0376f0e487b56e7d8a4d191
Bhaveen4 <bhaveenbhaskaran7@gmail.com>
2026-01-21 11:51:56 +0530

    data fetching

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -13,19 +13,19 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default async function CardWrapper() {
+export default async function CardWrapper({numberOfCustomers, totalPaidInvoices, totalPendingInvoices, numberOfInvoices} : {numberOfCustomers: number; totalPaidInvoices: string; totalPendingInvoices: string; numberOfInvoices: number;}) {
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
